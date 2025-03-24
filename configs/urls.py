9a76d53cb5377fcbb5df8522695496c67e716bfd
Alexandr Semenets <alexandrsemenec01@gmail.com>
2025-03-24 13:29:51 +0200

    [git init in branch lesson-2]

--- configs/urls.py
+++ configs/urls.py
@@ -2,16 +2,9 @@
 URL configuration for configs project.
 """
 from django.urls import path
-from myapp.views import MyAppView, MySecondAppView
 from users.views import UserListCreateView, UserRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('myapp', MyAppView.as_view()),
-
-    # path('mysecondapp/<int:age>', MySecondAppView.as_view()) # we can catch params and sent to MySecondAppView
-    path('mysecondapp/<int:age>/<str:name>/<slug:surname>', MySecondAppView.as_view()),
-    # slug don't allow set spaces, but can set underscore
-
     path('users', UserListCreateView.as_view()),
 
     path('users/<int:pk>', UserRetrieveUpdateDestroyView.as_view())
